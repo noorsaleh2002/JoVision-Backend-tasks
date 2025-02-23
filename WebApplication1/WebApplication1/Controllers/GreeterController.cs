edb58e2d45f6572aec829574aff4b84c4542972c
noorsaleh2002 <115577009+noorsaleh2002@users.noreply.github.com>
2025-02-23 21:58:51 +0300

    Task44

---
+++ WebApplication1/WebApplication1/Controllers/GreeterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreeterController : ControllerBase
+    {
+        [HttpGet]
+        public string GetGreeting([FromQuery] string name = "anonymous")
+        {
+            return $"Hello {name}";
+        }
+
+    }
+}
+
