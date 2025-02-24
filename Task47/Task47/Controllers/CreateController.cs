9b3b9147ebd048965cb4de4310899e5279e8428d
noorsaleh2002 <115577009+noorsaleh2002@users.noreply.github.com>
2025-02-24 16:04:23 +0300

    Task47

---
+++ Task47/Task47/Controllers/CreateController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Task47.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateController : ControllerBase
+    {
+        private readonly string _fileDirectory = Path.Combine(Directory.GetCurrentDirectory(), "UploadedFiles");
+        [HttpPost("create")]
+        public async Task<IActionResult> UploadFile(IFormFile file, [FromForm] string owner)
+        {
+            if (file == null || file.Length == 0 || string.IsNullOrEmpty(owner))
+            {
+                return BadRequest("File or Owner is missing.");
+            }
+            try
+            {
+                var filePath = Path.Combine(_fileDirectory, file.FileName);
+                if (System.IO.File.Exists(filePath))
+                {
+                    return BadRequest("File with the same name already exists.");
+                }
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);//saving the image
+                }
+                //create metadata for the image
+                var fileMetadata = new
+                {
+                    Owner = owner,
+                    CreationTime = DateTime.Now,
+                    LastModifiedTime = DateTime.Now
+
+                };
+                //saving metadata as JSON
+                var jsonMetaDataPath = Path.Combine(_fileDirectory, $"{Path.GetFileNameWithoutExtension(file.FileName)}.json");
+                System.IO.File.WriteAllText(jsonMetaDataPath, JsonConvert.SerializeObject(fileMetadata));
+                return Created($"api/file/{file.FileName}", new { message = "File uploaded successfully." });
+
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+
+        }
+
+
+    }
+}
---
+++ Task47/Task47/Controllers/DeleteController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace   Task47.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeleteController : ControllerBase
+    {
+        
+        private readonly string _fileDirectory = Path.Combine(Directory.GetCurrentDirectory(), "UploadedFiles");
+
+        
+        [HttpGet("delete")]
+        public IActionResult DeleteFile([FromQuery] string fileName, [FromQuery] string fileOwner)
+        {
+            if (string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(fileOwner))
+            {
+                return BadRequest("File name or owner missing.");
+            }
+
+            try
+            {
+                
+                var filePath = Path.Combine(_fileDirectory, fileName);
+                var jsonMetadataPath = Path.Combine(_fileDirectory, $"{Path.GetFileNameWithoutExtension(fileName)}.json");
+
+                
+                if (!System.IO.File.Exists(filePath) || !System.IO.File.Exists(jsonMetadataPath))
+                {
+                    return BadRequest("File or metadata does not exist.");
+                }
+
+                
+                var metadataContent = System.IO.File.ReadAllText(jsonMetadataPath);
+                dynamic metadata = JsonConvert.DeserializeObject(metadataContent);
+
+               
+                if (metadata.Owner != fileOwner)
+                {
+                    return Forbid("Owner name does not match.");
+                }
+
+                
+                System.IO.File.Delete(filePath);
+                System.IO.File.Delete(jsonMetadataPath);
+
+                return Ok(new { message = "File and metadata deleted successfully." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
